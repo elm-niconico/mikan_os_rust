9f585ec073933d712dadb8eb1cc0947567485797
elm-niconico <elmgameinfo@gmail.com>
2022-06-18 18:44:44 +0900

    add: configureレジスタの作成処理を追加しました。

---
+++ src/usb/xhci/registers/operators/create/configure.rs
@@ -0,0 +1,38 @@
+use crate::serial_println;
+use crate::usb::xhci::registers::create_type::{CreateRegisterResult, CreateType};
+use crate::usb::xhci::registers::operators::structs::configure::ConfigureRegister;
+use crate::usb::xhci::registers::register_info::RegisterInfo;
+use crate::utils::raw_ptr::transmute_from_u64;
+use crate::utils::test_fn::extract_operational_base_addr;
+
+
+pub trait ICreateConfigure {
+    fn new_configure(&self, operational_base: u64) -> CreateRegisterResult<ConfigureRegister>;
+}
+
+
+impl ICreateConfigure for CreateType {
+    fn new_configure(&self, operational_base: u64) -> CreateRegisterResult<ConfigureRegister> {
+        match self {
+            CreateType::UncheckTransmute => {uncheck_transmute(operational_base)},
+            _ => {todo!()}
+        }
+    }
+}
+
+
+fn uncheck_transmute(operational_base: u64) -> CreateRegisterResult<ConfigureRegister> {
+    let start_addr = operational_base + 0x38;
+    let configure = transmute_from_u64::<ConfigureRegister>(start_addr);
+    
+    Ok(RegisterInfo::new(start_addr, configure))
+}
+
+
+#[test_case]
+pub fn should_new_configure() {
+    let configure = uncheck_transmute(extract_operational_base_addr());
+    
+    assert!(configure.is_ok());
+    serial_println!("{:?}", configure.unwrap());
+}
--- src/usb/xhci/registers/operators/create/mod.rs
+++ src/usb/xhci/registers/operators/create/mod.rs
@@ -5,3 +5,4 @@ pub mod page_size;
 pub mod device_notify_control;
 pub mod command_ring_control;
 pub mod device_context_base_array_pointer;
+pub mod configure;
